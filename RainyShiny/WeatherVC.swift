3880b61f671063ce1fffb3b884bfb371b4b5d679
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-06-08 22:47:07 -0500

    started added constraints to uilabels

---
+++ RainyShiny/WeatherVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RainyShiny
+//
+//  Created by Ricardo Herrera Petit on 6/6/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+

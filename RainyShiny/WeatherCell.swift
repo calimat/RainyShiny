6b1a6935048eda83e9828c0bf5da4b18dd883761
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-06-14 23:49:33 -0500

    added configuration for weather cell... But returning blank cells

---
+++ RainyShiny/WeatherCell.swift
@@ -0,0 +1,39 @@
+//
+//  WeatherCell.swift
+//  RainyShiny
+//
+//  Created by Ricardo Herrera Petit on 6/14/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    @IBOutlet weak var weatherIcon: UIImageView!
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    @IBOutlet weak var weatherType: UILabel!
+    
+    @IBOutlet weak var highTemp: UILabel!
+    
+    @IBOutlet weak var lowTemp: UILabel!
+    
+
+    func configureCell(forecast:Forecast) {
+        
+        lowTemp.text = "\(forecast.lowTemp)"
+        highTemp.text = "\(forecast.highTemp)"
+        weatherType.text = forecast.weatherType
+        
+        weatherIcon.image = UIImage(named:forecast.weatherType)
+        
+        dayLabel.text = forecast.date
+        
+        
+    }
+
+  
+
+}
--- RainyShiny/WeatherVC.swift
+++ RainyShiny/WeatherVC.swift
@@ -80,15 +80,22 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return forecasts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as? WeatherCell {
+            let forecast = forecasts[indexPath.row]
+            cell.configureCell(forecast: forecast)
+            return cell
+        } else {
+            return WeatherCell()
+        }
         
-        return cell 
+      
     }
 
+    
 
     func updateMainUI() {
         dateLabel.text = currentWeather.date
